2ee553c5a5d9e73e8e11855a21fe3aa3d0fe0e99
aedi <none>
2016-05-26 03:38:55 -0500

    DAO Local. Funciones abstractas: questUser addUser editUser listUser

--- src/dao/l/UsuarioDAOL.java
+++ src/dao/l/UsuarioDAOL.java
@@ -1,7 +1,13 @@
 package dao.l;
 
+import java.util.List;
+
 import model.Usuario;
 
 public interface UsuarioDAOL {
 	public abstract Usuario validUser(Usuario obj) throws Exception; 
+	public abstract Usuario questUser(Usuario obj) throws Exception;
+	public abstract void addUser(Usuario obj) throws Exception;
+	public abstract void editUser(Usuario obj) throws Exception;
+	public abstract List<Usuario> listUser(Usuario obj) throws Exception;
 }
